2f3681455084540808e5bcb4a479f77c655d0a4a
Hammarok <caioamarocci@gmail.com>
2024-12-02 16:42:59 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,14 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+}
